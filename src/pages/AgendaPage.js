ea740102770331ca30c76f468d63fb229fdc0f31
Thomas D <tromatik@gmail.com>
2021-05-27 15:49:48 +0200

    Allow agenda page direct loading

--- src/pages/AgendaPage.js
+++ src/pages/AgendaPage.js
@@ -9,9 +9,55 @@ import "../css/site.css"
 
 import { close_icon } from "../components/jss/cvcss"
 
-const AgendaPage = () => {
-  const [slots, setSlots] = useState([])
+import { useStaticQuery, graphql } from "gatsby"
 
+const AgendaPage = () => {
+  const [agendaSlots, setSlots] = useState([])
+  const data = useStaticQuery(graphql`
+    query AgendaQuery {
+      allAgendaJson {
+        nodes {
+          slots {
+            eventType
+            img
+            slotId
+            timeEnd
+            timeStart
+            slotFeedBack
+            tracks {
+              feedBack
+              selectedFlag
+              speakers {
+                designation
+                externalLink
+                firstName
+                lastName
+                profilePicture
+              }
+              title
+              trackId
+              trackLength
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // if (typeof window !== "undefined" && localStorage.getItem('slots') !== null) {
+  //   window.onload = localStorage.removeItem('slots');
+  //  }
+  
+  function SetLocalStorage(key, value) {
+    // Do not remove "typeof" : https://github.com/gatsbyjs/gatsby/issues/14480#issuecomment-497983196
+    if (typeof window !== "undefined" && localStorage.getItem(key) === null) {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+  }
+
+  const { slots } = data.allAgendaJson.nodes[0]
+  SetLocalStorage("slots", agendaSlots)
+  
   useEffect(() => {
     setSlots(JSON.parse(localStorage.getItem("slots")))
   }, [])
